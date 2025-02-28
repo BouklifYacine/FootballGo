e0d4ed53ebffac542957bf5e085762e21ec7b2d9
YacineBoukliff <yacine69800@live.fr>
2025-02-28 13:48:58 +0100

    Création route patch pour changer les données de l'équipe

--- FootballGo/app/api/equipes/[id]/route.ts
+++ FootballGo/app/api/equipes/[id]/route.ts
@@ -1,12 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/prisma";
+import { CreationEquipeSchema } from "@/app/(schema)/SchemaEquipe";
 
 export async function GET(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   const { id } = await params;
-  const idUtilisateur = "cm7ojx8uk0000ir5o5gd422s";
+  const idUtilisateur = "cm7ojx8uk0000ir5o5gd422sr";
 
   try {
     const equipe = await prisma.equipe.findUnique({
@@ -48,5 +49,70 @@ export async function GET(
   } catch (error) {
     console.error("Erreur lors de la recherche d'équipe:", error);
     return NextResponse.json({ message: "Erreur serveur" }, { status: 500 });
+  }
+}
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { nom, logoUrl, description } = await request.json();
+    const { id } = await params;
+    const idUtilisateur = "cm7ojx8uk0000ir5o5gd422sr";
+
+    const validation = CreationEquipeSchema.safeParse({
+      nom,
+      logoUrl,
+      description,
+    });
+
+    if (!validation.success) {
+      return NextResponse.json(
+        {
+          message: "Données invalides",
+          erreurs: validation.error.format(),
+        },
+        { status: 400 }
+      );
+    }
+
+    const equipe = await prisma.equipe.findUnique({
+      where: { id },
+    });
+
+    if (!equipe) {
+      return NextResponse.json(
+        { message: "Équipe non trouvée" },
+        { status: 404 }
+      );
+    }
+
+    const membreEquipe = await prisma.membreEquipe.findFirst({
+      where: {
+        userId: idUtilisateur,
+        equipeId: id,
+        role: "ENTRAINEUR",
+      },
+    });
+
+    if (!membreEquipe) {
+      return NextResponse.json( {message: "Seuls les coach de cette équipe peuvent modifier les données du club"}, { status: 403 } )}
+
+      const nouvelledonnesEquipe = await prisma.equipe.update({
+        where : {id}, 
+        data : {
+            nom : nom, 
+            description : description, 
+            logoUrl : logoUrl
+        }
+      })
+
+      return NextResponse.json({message : "Changements effectué" , nouvelledonnesEquipe },  {status : 200 })
+  } catch (error) {
+    console.log(
+      "Erreur lors de la modification des infos de votre équipe ",
+      error
+    );
   }
 }
