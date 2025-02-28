8523bba3b9ab497e6adb10ebeeeb71b883e5071b
YacineBoukliff <yacine69800@live.fr>
2025-02-28 10:51:14 +0100

    Création de récupération de toute les équipes je dois faire les autre api post put et delete et après plus tard faire les servers actions pour les mutations et garder que les api en GET

---
+++ FootballGo/app/api/equipes/route.ts
@@ -0,0 +1,21 @@
+import { auth } from "@/auth";
+import { prisma } from "@/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+
+  const session = await auth();
+
+  if (!session?.user?.id)
+    return NextResponse.json({
+      message: "Vous devez etre connecté pour accéder a ces infos "} , {status : 400});
+      
+  const equipe = await prisma.equipe.findMany();
+
+  if (equipe.length === 0)
+    return NextResponse.json({
+      message: "Il n'y a pas d'équipe disponible" 
+    }, {status : 400});
+
+  return NextResponse.json(equipe);
+}
---
+++ FootballGo/prisma/migrations/20250228091036_roleequipe/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "roleEquipe" "RoleEquipe" NOT NULL DEFAULT 'JOUEUR';
