4d020d205b18975eea1181549dbac2b0de84b30d
YacineBoukliff <yacine69800@live.fr>
2025-02-28 17:56:19 +0100

    Route DELETE pour la suppression du code d'invitation de l'équipe

--- FootballGo/app/api/equipes/[id]/codeinvitation/route.ts
+++ FootballGo/app/api/equipes/[id]/codeinvitation/route.ts
@@ -67,3 +67,57 @@ export async function GET(request: NextRequest, { params }: RouteParams) {
     return NextResponse.json({ message: "Erreur serveur" }, { status: 500 });
   }
 }
+
+export async function DELETE(request: NextRequest, { params }: RouteParams) {
+  const { id } = await params;
+  const idUtilisateur = "cm7oz4qau0000irj0fl31bdp9";
+
+  try {
+    const equipe = await prisma.equipe.findUnique({
+      where: { id },
+    });
+
+    if (!equipe) {
+      return NextResponse.json(
+        { message: "Équipe non trouvée" },
+        { status: 404 }
+      );
+    }
+
+    const membreEquipe = await prisma.membreEquipe.findFirst({
+      where: {
+        userId: idUtilisateur,
+        equipeId: id,
+        role: "ENTRAINEUR",
+      },
+    });
+
+    if (!membreEquipe) {
+      return NextResponse.json(
+        {
+          message:
+            "Seuls les coachs de cette équipe peuvent accéder à cette ressource",
+        },
+        { status: 403 }
+      );
+    }
+
+    if (!equipe.codeInvitation)
+      return NextResponse.json({
+        message: `Code d'invitation inexistant pour l'équipe ${equipe.nom}`,
+      });
+
+    await prisma.equipe.update({
+      where: { id },
+      data: { codeInvitation: null },
+    });
+
+    return NextResponse.json({message : `Code d'invitation de l'équipe ${equipe.nom} supprimé`})
+  } catch (error) {
+    console.error(
+      "Erreur serveur lors de la récupération du code d'invitation",
+      error
+    );
+    return NextResponse.json({ message: "Erreur serveur" }, { status: 500 });
+  }
+}
