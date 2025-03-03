61a99e3d190d1543d681f98054388b916d834644
YacineBoukliff <yacine69800@live.fr>
2025-03-03 13:05:17 +0100

    Création de la route delete des stats d'un joueur pour bien supprimer les stats d'un match

--- FootballGo/app/api/evenement/[id]/statistiques/joueurs/[joueurId]/route.ts
+++ FootballGo/app/api/evenement/[id]/statistiques/joueurs/[joueurId]/route.ts
@@ -11,7 +11,7 @@ interface RouteParams {
 }
 
 dayjs.locale("fr");
-const idUtilisateur = "cm7sthoee0001irowu7bczcj";
+const idUtilisateur = "cm7sthoee0001irowu7bczcjg";
 
 export async function PATCH(request: NextRequest, { params }: RouteParams) {
   const { id, joueurId } = await params;
@@ -109,6 +109,95 @@ export async function PATCH(request: NextRequest, { params }: RouteParams) {
     });
 
     return NextResponse.json(nouvellesStats, { status: 200 });
+  } catch (error) {
+    console.error("Erreur lors de la mise à jour des statistiques:", error);
+    return NextResponse.json(
+      { message: "Erreur lors de la mise à jour des statistiques" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(request: NextRequest, { params }: RouteParams) {
+  const { id, joueurId } = await params;
+
+  if (joueurId !== idUtilisateur) {
+    return NextResponse.json(
+      { message: "Vous ne pouvez modifier que vos propres statistiques" },
+      { status: 403 }
+    );
+  }
+
+  const evenementMatch = await prisma.evenement.findUnique({
+    where: { id: id, typeEvenement: "MATCH" },
+    include: { equipe: true },
+  });
+
+  if (!evenementMatch)
+    return NextResponse.json(
+      { message: "L'événement n'existe pas ou c'est un entrainement" },
+      { status: 400 }
+    );
+
+  const debutPlus2Heures = dayjs(evenementMatch.dateDebut).add(2, "hour");
+  const maintenant = dayjs();
+
+  if (maintenant.isBefore(debutPlus2Heures))
+    return NextResponse.json(
+      {
+        message:
+          "Les statistiques ne peuvent être modifiées que 2 heures après le début de la rencontre",
+      },
+      { status: 403 }
+    );
+
+  const joueurEquipe = await prisma.membreEquipe.findFirst({
+    where: {
+      userId: idUtilisateur,
+      equipeId: evenementMatch.equipeId,
+    },
+  });
+
+  if (!joueurEquipe)
+    return NextResponse.json(
+      { message: "Vous n'êtes pas membre de cette équipe" },
+      { status: 403 }
+    );
+
+  const joueurPresent = await prisma.presence.findFirst({
+    where: {
+      userId: idUtilisateur,
+      evenementId: id,
+      statut: "PRESENT",
+    },
+  });
+
+  if (!joueurPresent) {
+    return NextResponse.json(
+      { message: "Vous n'êtes pas marqué comme présent à ce match" },
+      { status: 403 }
+    );
+  }
+
+  const statsExistantes = await prisma.statistiqueJoueur.findFirst({
+    where: {
+      evenementId: id,
+      userId: idUtilisateur,
+    },
+  });
+
+  if (!statsExistantes)
+    return NextResponse.json(
+      { message: "Aucune statistique n'existe pour vous et ce match" },
+      { status: 404 }
+    );
+
+  try {
+    await prisma.statistiqueJoueur.delete({
+      where: { id: statsExistantes.id },
+    });
+
+    return NextResponse.json({message : "Votre stats a bien été supprimé"}, { status: 200 });
   } catch (error) {
     console.error("Erreur lors de la mise à jour des statistiques:", error);
     return NextResponse.json(
