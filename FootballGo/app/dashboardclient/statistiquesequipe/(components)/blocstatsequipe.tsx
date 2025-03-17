9bccbaa395e53e21fed3d7c4f9b062cfcc23a0e0
YacineBoukliff <yacine69800@live.fr>
2025-03-17 12:13:51 +0100

    Amélioration de l'ui des blocs stats

--- FootballGo/app/dashboardclient/statistiquesequipe/(components)/blocstatsequipe.tsx
+++ FootballGo/app/dashboardclient/statistiquesequipe/(components)/blocstatsequipe.tsx
@@ -1,11 +1,13 @@
 "use client"
 
-import { Activity, Award, Clock, Zap, Users } from "lucide-react";
+import {  Award,  ChartNoAxesCombined, ThumbsUp, ThumbsDown } from "lucide-react";
 import React from "react";
-
 import { useSession } from "next-auth/react";
 import Loading from "../../(components)/Loading";
 import { useStatistiqueEquipeFDP } from "../(hook)/useStatistiqueEquipe";
+
+import { TbPlayFootball } from "react-icons/tb";
+import { IoIosFootball } from "react-icons/io";
 
 const BlocStatsEquipe = () => {
   const { data: session } = useSession();
@@ -13,118 +15,106 @@ const BlocStatsEquipe = () => {
   const sessionID = session?.user?.id || "";
   const { data, isPending, error } = useStatistiqueEquipeFDP(sessionID);
 
-  console.log("Buts marqué:", data?.butsMarques);
-
   if(isPending) return <><Loading></Loading></>
 
   if (error) return <div> Les données sont indisponible </div>
   return (
     
     <>
-      <div className="flex flex-col md:flex-row justify-center gap-6 lg:gap-12 items-center p-4">
-        <div className="bloc1 rounded-2xl w-full md:w-72 bg-blue-500 space-y-4 p-4">
-
-            {}
+    <div className="flex flex-col md:flex-row justify-center gap-6 lg:gap-12 items-center p-4">
+        <div className="bloc1 rounded-2xl w-full md:w-72 bg-white border border-gray-300 shadow-sm space-y-4 p-4">
           <div className="flex items-center justify-between">
-            <p className="text-lg font-medium">Victoire</p>
-            <span className="block md:hidden">
-              <Activity size={30} />
+            <p className="text-lg font-medium text-gray-800">Victoires</p>
+            <span className="block md:hidden text-gray-600">
+            <ThumbsUp size={32} absoluteStrokeWidth className="text-green-500" />
             </span>
-            <span className="hidden md:block">
-              <Activity size={40} />
+            <span className="hidden md:block text-gray-600">
+            <ThumbsUp size={40} absoluteStrokeWidth className="text-green-500" />
             </span>
           </div>
-          <div className="pt-4">
-            <p className="font-bold text-lg tracking-tighter md:text-4xl">
+          <div className="pt-6">
+            <p className="font-bold text-2xl tracking-tighter md:text-4xl text-green-500">
               {data.victoires}
             </p>
           </div>
-          <div className="flex gap-6">
-            <p>Yacine</p>
-            <p>+35</p>
-          </div>
+
+          <p className="text-gray-600">Cette saison</p>
         </div>
 
-        <div className="bloc1 rounded-2xl w-full md:w-72 bg-blue-500 space-y-4 p-4">
+        <div className="bloc1 rounded-2xl w-full md:w-72 bg-white border border-gray-300 shadow-sm space-y-4 p-4">
           <div className="flex items-center justify-between">
-            <p className="text-lg font-medium">Defaites</p>
-            <span className="block md:hidden">
-              <Zap size={30} />
+            <p className="text-lg font-medium text-gray-800">Nuls</p>
+            <span className="block md:hidden text-gray-600">
+              <TbPlayFootball size={30} />
             </span>
-            <span className="hidden md:block">
-              <Zap size={40} />
+            <span className="hidden md:block text-gray-600">
+              <TbPlayFootball size={40} />
             </span>
           </div>
-          <div className="pt-4">
-            <p className="font-bold text-lg tracking-tighter md:text-4xl">
+          <div className="pt-6">
+            <p className="font-bold text-2xl tracking-tighter md:text-4xl text-gray-900">
+              {data.nuls}
+            </p>
+          </div>
+
+          <p className="text-gray-600">Cette saison</p>
+        </div>
+
+        <div className="bloc1 rounded-2xl w-full md:w-72 bg-white border border-gray-300 shadow-sm space-y-4 p-4">
+          <div className="flex items-center justify-between">
+            <p className="text-lg font-medium text-gray-800">Défaites</p>
+            <span className="block md:hidden text-gray-600">
+            <ThumbsDown size={32} absoluteStrokeWidth className="text-red-500" />
+            </span>
+            <span className="hidden md:block text-gray-600">
+            <ThumbsDown size={40} absoluteStrokeWidth className="text-red-500" />
+            </span>
+          </div>
+          <div className="pt-6">
+            <p className="font-bold text-2xl tracking-tighter md:text-4xl text-red-500">
               {data.defaites}
             </p>
           </div>
-          <div className="flex gap-6">
-            <p>Kylian</p>
-            <p>+12</p>
-          </div>
+
+          <p className="text-gray-600">Cette saison</p>
         </div>
 
-        <div className="bloc1 rounded-2xl w-full md:w-72 bg-blue-500 space-y-4 p-4">
+        <div className="bloc1 rounded-2xl w-full md:w-72 bg-white border border-gray-300 shadow-sm space-y-4 p-4">
           <div className="flex items-center justify-between">
-            <p className="text-lg font-medium">NUL</p>
-            <span className="block md:hidden">
-              <Clock size={30} />
+            <p className="text-lg font-medium text-gray-800">Buts Inscrit</p>
+            <span className="block md:hidden text-gray-600">
+            <IoIosFootball size={30} className="text-green-500"/>
             </span>
-            <span className="hidden md:block">
-              <Clock size={40} />
+            <span className="hidden md:block text-gray-600">
+            <IoIosFootball size={40} className="text-green-500"/>
             </span>
           </div>
-          <div className="pt-4">
-            <p className="font-bold text-lg tracking-tighter md:text-4xl">
-             {data.nuls}
+          <div className="pt-6">
+            <p className="font-bold text-2xl tracking-tighter md:text-4xl text-green-500">
+              {data.butsMarques}
             </p>
           </div>
-          <div className="flex gap-6">
-            <p>Ali</p>
-            <p>+90</p>
-          </div>
+
+          <p className="text-gray-600">Cette saison</p>
         </div>
 
-        <div className="bloc1 rounded-2xl w-full md:w-72 bg-blue-500 space-y-4 p-4">
+        <div className="bloc1 rounded-2xl w-full md:w-72 bg-white border border-gray-300 shadow-sm space-y-4 p-4">
           <div className="flex items-center justify-between">
-            <p className="text-lg font-medium">Buts inscrits</p>
-            <span className="block md:hidden">
-              <Award size={30} />
+            <p className="text-lg font-medium text-gray-800">Buts encaissés</p>
+            <span className="block md:hidden text-gray-600">
+            <IoIosFootball size={30} className="text-red-500"/>
             </span>
-            <span className="hidden md:block">
-              <Award size={40} />
-            </span>
-          </div>
-          <div className="pt-4">
-            <p className="font-bold text-lg tracking-tighter md:text-4xl">{data.butsMarques}</p>
-          </div>
-          <div className="flex gap-6">
-            <p>Équipe</p>
-            <p>+3</p>
-          </div>
-        </div>
-
-        <div className="bloc1 rounded-2xl w-full md:w-72 bg-blue-500 space-y-4 p-4">
-          <div className="flex items-center justify-between">
-            <p className="text-lg font-medium">Buts encaissés</p>
-            <span className="block md:hidden">
-              <Users size={30} />
-            </span>
-            <span className="hidden md:block">
-              <Users size={40} />
+            <span className="hidden md:block text-gray-600">
+            <IoIosFootball size={40} className="text-red-500" />
             </span>
           </div>
-          <div className="pt-4">
-            <p className="font-bold text-2xl tracking-tighter md:text-4xl">
-              {data.tauxVictoire }
+          <div className="pt-6">
+            <p className="font-bold text-2xl tracking-tighter md:text-4xl text-red-500">
+              {data.butsEncaisses}
             </p>
           </div>
-          <div className="flex gap-6">
-            <p>Effectif</p>
-            <p>+2</p>
-          </div>
+
+          <p className="text-gray-600">Cette saison</p>
         </div>
       </div>
     </>
